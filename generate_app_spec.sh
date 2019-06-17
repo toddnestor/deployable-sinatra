43dbdcb9cf80ac0a1b08303af636d2a4e121d27c
Todd D Nestor <todd.nestor@gmail.com>
2019-06-17 09:36:13 -0700

    rename AppSpec.yml to appspec.yml

--- generate_app_spec.sh
+++ generate_app_spec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf 'version: %s\nResources:\n  -TargetService:\n    Type: AWS::ECS::Service\n    Properties:\n    TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:22"\n' $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION > AppSpec.yml
+printf 'version: %s\nResources:\n  -TargetService:\n    Type: AWS::ECS::Service\n    Properties:\n    TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:22"\n' $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION > appspec.yml
