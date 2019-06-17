d0132c968b1e7dea81f594e4dac1f0f6386206a3
Todd D Nestor <todd.nestor@gmail.com>
2019-06-17 15:52:41 -0700

    have better formatted yaml

--- generate_app_spec.sh
+++ generate_app_spec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf 'version: %s\nResources:\n  -TargetService:\n    Type: AWS::ECS::Service\n    Properties:\n    TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:22"\n' $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION > appspec.yml
+printf 'version: %s\nResources:\n  - TargetService:\n      Type: AWS::ECS::Service\n      Properties:\n        TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:22"\n        LoadBalancerInfo:\n          ContainerName: "development-sinatra"\n          ContainerPort: "4000"\n        PlatformVersion: "LATEST"\n' $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION > appspec.yml
