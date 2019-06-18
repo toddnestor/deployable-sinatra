875ce1be8ce285dd52f3d439f57b5bce66d3e607
Todd D Nestor <todd.nestor@gmail.com>
2019-06-18 10:03:14 -0700

    try using .yaml extension

--- generate_app_spec.sh
+++ generate_app_spec.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+
+
 read -r -d '' APP_SPEC <<TEST
 version: %s
 Resources:
   - TargetService:
       Type: AWS::ECS::Service
       Properties:
-        TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:32"
+        TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:34"
         LoadBalancerInfo:
           ContainerName: "%s"
           ContainerPort: "4000"
         PlatformVersion: "LATEST"
 TEST
 
-printf "${APP_SPEC}\n" $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION $IMAGE_TAG > appspec.yml
+printf "${APP_SPEC}\n" $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION $IMAGE_TAG > appspec.yaml
