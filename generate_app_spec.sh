2c2c991e9e8ed04a2b4916869a0da74a034aeeb7
Todd D Nestor <todd.nestor@gmail.com>
2019-06-22 01:34:55 -0700

    we might deploy the correct task definition now

--- generate_app_spec.sh
+++ generate_app_spec.sh
@@ -8,11 +8,11 @@ Resources:
   - TargetService:
       Type: AWS::ECS::Service
       Properties:
-        TaskDefinition: "arn:aws:ecs:us-east-2:591328757508:task-definition/development-sinatra:34"
+        TaskDefinition: %s
         LoadBalancerInfo:
           ContainerName: "development-sinatra"
           ContainerPort: "4000"
         PlatformVersion: "LATEST"
 APP_SPEC_TEXT
 
-printf "${APP_SPEC}\n" $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION $IMAGE_TAG > appspec.yaml
+printf "${APP_SPEC}\n" $IMAGE_TAG-$CODEBUILD_RESOLVED_SOURCE_VERSION $TASK_DEFINITION > appspec.yaml
