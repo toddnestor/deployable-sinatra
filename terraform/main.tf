34b58e9006a841c900fe109e832b6da6e19931bf
Todd D Nestor <todd.nestor@gmail.com>
2019-06-25 07:14:09 -0700

    try it with so many things

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-2"
+}
+
 provider "godaddy" {
 }
 
@@ -38,6 +44,50 @@ module "staging-sinatra" {
   }
 }
 
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = ["subnet-317cbe4b", "subnet-671c222a"]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_rds_cluster" "postgresql" {
+  cluster_identifier      = "${var.environment}-minerva"
+  engine                  = "aurora-postgresql"
+  availability_zones      = ["us-east-2c"]
+  database_name           = "minerva"
+  master_username         = "minerva"
+  master_password         = "student9"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  db_subnet_group_name    = "${aws_db_subnet_group.default.name}"
+  skip_final_snapshot     = true
+}
+
+module "minerva" {
+  source = "./modules/containerized-service"
+
+  aws_access_key = "${var.aws_access_key}"
+  aws_secret_key = "${var.aws_secret_key}"
+  github_token = "${var.github_token}"
+
+  name = "minerva"
+  environment = "${var.environment}"
+  git_branch = "develop"
+  internal = false
+  repo_owner = "RaiseMe"
+
+  environment_variables = {
+    RACK_ENV = "${var.environment}"
+    DB_USER = "${aws_rds_cluster.postgresql.master_username}"
+    DB_PASSWORD = "${aws_rds_cluster.postgresql.master_password}"
+    DB_NAME = "${aws_rds_cluster.postgresql.database_name}"
+    DB_HOST = "${aws_rds_cluster.postgresql.endpoint}"
+  }
+}
+
 # GODADDY DNS #
 
 resource "godaddy_domain_record" "subdomain" {
@@ -56,33 +106,11 @@ resource "godaddy_domain_record" "subdomain" {
     data = "${module.staging-sinatra.alb_dns}"
     ttl = 600
   }
-}
 
-//resource "aws_rds_cluster" "postgresql" {
-//  cluster_identifier      = "${var.environment}-minerva"
-//  engine                  = "aurora-postgresql"
-//  availability_zones      = ["us-east-1c", "us-east-1d"]
-//  database_name           = "minerva"
-//  master_username         = "testuser"
-//  master_password         = "testpassword"
-//  backup_retention_period = 5
-//  preferred_backup_window = "07:00-09:00"
-//  vpc_security_group_ids = ["whatever", "they", "are"]
-//}
-//
-//module "minerva" {
-//  source = "../modules/containerized-service"
-//
-//  name = "minerva"
-//  environment = "${var.environment}"
-//  git_branch = "master"
-//  internal = false
-//
-//  environment_variables = {
-//    RACK_ENV = "${var.environment}"
-//    DB_USER = "${aws_rds_cluster.postgresql.master_username}"
-//    DB_PASSWORD = "${aws_rds_cluster.postgresql.master_password}"
-//    DB_NAME = "${aws_rds_cluster.postgresql.database_name}"
-//    DB_HOST = "${aws_rds_cluster.postgresql.endpoint}"
-//  }
-//}
+  record {
+    name = "${module.minerva.name}"
+    type = "CNAME"
+    data = "${module.minerva.alb_dns}"
+    ttl = 600
+  }
+}
